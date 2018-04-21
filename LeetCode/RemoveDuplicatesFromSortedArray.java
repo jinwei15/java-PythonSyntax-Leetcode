6314edb21d021ded1ae64377c7eb8ac85c909b72
Jinwei Zhang <jinweizhang@JinweiMAC.local>
2018-04-21 16:10:47 +0100

    all one question to the github

--- LeetCode/RemoveDuplicatesFromSortedArray.java
+++ LeetCode/RemoveDuplicatesFromSortedArray.java
@@ -1,4 +1,43 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class RemoveDuplicatesFromSortedArray {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
+	}
+}
+
+class SolutionRemoveDuplicatesFromSortedArray {
+	// public int removeDuplicates(int[] nums) {
+	// int end = nums.length;
+	// Set<Integer> set = new HashSet<Integer>();
+	//
+	// for(int i = 0; i < end; i++){
+	// set.add(nums[i]);
+	// }
+	//
+	//
+	// int[] y = new int[set.size()];
+	// int c = 0;
+	// for(int x : set) y[c++] = x;
+	//
+	// return set.size();
+	// }
+
+	public int removeDuplicates(int[] nums) {
+		if (nums.length == 0)
+			return 0;
+		int i = 0;
+		for (int j = 1; j < nums.length; j++) {
+			if (nums[j] != nums[i]) {
+				i++;
+				nums[i] = nums[j];
+			}
+		}
+		return i + 1;
+	}
 }
