57f596fe39c6a7465d1deacee93fdcc2dc67ed85
jinwei14 <janviezhang14@gmail.com>
2018-04-24 13:26:55 +0100

    commit to implement strStr()  aka indexof()

---
+++ LeetCode/src/ImplementStrStr.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+
+/**
+ * @author jinweizhang
+ *
+ */
+public class ImplementStrStr {
+
+	/**
+	 * @param args
+	 * 
+	 *            Implement strStr().
+	 * 
+	 *            Return the index of the first occurrence of needle in haystack, or
+	 *            -1 if needle is not part of haystack.
+	 * 
+	 *            Example 1:
+	 * 
+	 *            Input: haystack = "hello", needle = "ll" Output: 2 Example 2:
+	 * 
+	 *            Input: haystack = "aaaaa", needle = "bba" Output: -1
+	 *            Clarification:
+	 * 
+	 *            What should we return when needle is an empty string? This is a
+	 *            great question to ask during an interview.
+	 * 
+	 *            For the purpose of this problem, we will return 0 when needle is
+	 *            an empty string. This is consistent to C's strstr() and Java's
+	 *            indexOf().
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String haystack = "hello";
+		String needle = "llo";
+
+		int aa = haystack.indexOf(needle);
+		System.out.println(aa);
+	}
+
+}
+
+
+//easy solution git hub !!!!
+class SolutionImplementStrStr {
+	public int strStr(String haystack, String needle) {
+		for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            
+			if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                   return i;
+			}
+		}
+		return -1;
+
+	}
+}
--- LeetCode/src/RemoveDuplicatesFromSortedArray.java
+++ LeetCode/src/RemoveDuplicatesFromSortedArray.java
@@ -14,24 +14,22 @@ public class RemoveDuplicatesFromSortedArray {
 }
 
 class SolutionRemoveDuplicatesFromSortedArray {
-	//this is the first try of the solution
-	public int removeDuplicates(int[] nums) {
-	int end = nums.length;
-	Set<Integer> set = new HashSet<Integer>();
+	// public int removeDuplicates(int[] nums) {
+	// int end = nums.length;
+	// Set<Integer> set = new HashSet<Integer>();
+	//
+	// for(int i = 0; i < end; i++){
+	// set.add(nums[i]);
+	// }
+	//
+	//
+	// int[] y = new int[set.size()];
+	// int c = 0;
+	// for(int x : set) y[c++] = x;
+	//
+	// return set.size();
+	// }
 
-	for(int i = 0; i < end; i++){
-	set.add(nums[i]);
-	}
-
-
-	int[] y = new int[set.size()];
-	int c = 0;
-	for(int x : set) y[c++] = x;
-
-	return set.size();
-	}
-
-//this is the standard solution 
 	public int removeDuplicates(int[] nums) {
 		if (nums.length == 0)
 			return 0;
@@ -42,11 +40,11 @@ class SolutionRemoveDuplicatesFromSortedArray {
 				nums[i] = nums[j];
 			}
 		}
-
+		
 		for (int j = 0; j < nums.length; j++) {
 			System.out.print(nums[j]);
 		}
-
+		
 		return i + 1;
 	}
 }
