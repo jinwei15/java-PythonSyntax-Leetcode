d53e97a2598472e3e1b2a76afcaa9b87923671e9
jinwei14 <janviezhang14@gmail.com>
2018-05-03 20:45:59 +0100

    reverse string

--- LeetCode/src/LongestCommonPrefix.java
+++ LeetCode/src/LongestCommonPrefix.java
@@ -103,3 +103,4 @@ class SolutionLong2 {
 		return prefix;
 	}
 }
+
---
+++ LeetCode/src/ReverseString.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+/**
+ * @author jinweizhang
+ *
+ */
+public class ReverseString {
+
+	/**
+	 * @param args
+	 *            Write a function that takes a string as input and returns the
+	 *            string reversed.
+	 * 
+	 *            Example: Given s = "hello", return "olleh".
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class ReverseStringSolution {
+	public String reverseString(String s) {
+		return new StringBuilder(s).reverse().toString();
+	}
+}
