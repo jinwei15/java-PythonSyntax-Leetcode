cd1daa7e2e5aeb0177bbefb9394c23e2f0e329da
jinwei.zhang <janviezhang14@gmail.com>
2018-06-12 12:01:41 +0100

    Update RotateArray.java
    rotate array

--- LeetCode/src/RotateArray.java
+++ LeetCode/src/RotateArray.java
@@ -1,11 +1,11 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int[] onHold = new int [nums.length];
-        
-        //index from k to the very beginning.
-        for (int i = nums.length; i--; i<=k){
-            
-            
+         int[] a = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            a[(i + k) % nums.length] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = a[i];
         }
     }
 }
