f4f94c59863bdd37b5e3bd0a117c2ee4259b9f83
jinwei.zhang <janviezhang14@gmail.com>
2018-06-12 11:44:41 +0100

    Create reverseAString.java

---
+++ LeetCode/src/reverseAString.java
@@ -0,0 +1,10 @@
+//1.Converting String into Bytes
+
+//2.Using built in reverse() method of the StringBuilder class
+
+//3.Converting String to character array:
+
+//4.Convert the input string into character array by using the toCharArray()
+
+
+//5.Using ArrayList object:
