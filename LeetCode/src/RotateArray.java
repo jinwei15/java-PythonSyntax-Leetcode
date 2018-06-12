6fe45a4d5ee3d14b868a0f1eca64fb273668708b
jinwei.zhang <janviezhang14@gmail.com>
2018-06-12 11:32:37 +0100

    Create RotateArray.java
    new

---
+++ LeetCode/src/RotateArray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int[] onHold = new int [nums.length];
+        
+        //index from k to the very beginning.
+        for (int i = nums.length; i--; i<=k){
+            
+            
+        }
+    }
+}
