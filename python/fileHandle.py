ce2027c5363d57023e835167793d8c1d4d202032
jinwei14 <janviezhang14@gmail.com>
2018-09-22 16:34:17 +0100

    add file handling code

--- python/fileHandle.py
+++ python/fileHandle.py
@@ -57,3 +57,36 @@ for line in fhand:
 print('There were', count, 'subject lines in', fname)
 
 # Code: http://www.py4e.com/code3/search7.py
+
+
+"""
+7.1 Write a program that prompts for a file name,
+then opens that file and reads through the file,
+and print the contents of the file in upper case.
+Use the file words.txt to produce the output below.
+You can download the sample data at http://www.py4e.com/code3/words.txt
+"""
+
+# Use words.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+
+for line in fh:
+    line = line.rstrip()
+    print(line.upper())
+
+
+"""
+
+Write a program that prompts for a file name, then opens that file and reads through the file,
+looking for lines of the form:
+
+X-DSPAM-Confidence:    0.8475
+Count these lines and extract the floating point values from each of the lines
+and compute the average of those values and produce an output as shown below.
+Do not use the sum() function or a variable named sum in your solution.
+You can download the sample data at http://www.py4e.com/code3/mbox-short.txt
+when you are testing below enter mbox-short.txt as the file name.
+
+
+"""
