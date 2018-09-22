7a5a493828029977e2129766e12af706e8f8f64d
jinwei14 <janviezhang14@gmail.com>
2018-09-22 15:55:04 +0100

    file handling 2

--- python/fileHandle.py
+++ python/fileHandle.py
@@ -1,4 +1,59 @@
 # https://www.py4e.com/html3/07-files
+# the file handle is sequence of strings
 fhand = open('stuff.txt')
 print(fhand)
 #<_io.TextIOWrapper name='mbox.txt' mode='r' encoding='cp1252'>
+
+#new line ####################################### this in the file
+ stuff = 'Hello\nWorld!'
+# >>> stuff
+# 'Hello\nWorld!'
+print(stuff)
+Hello
+World!
+stuff = 'X\nY'
+print(stuff)
+# X
+# Y
+len(stuff)
+#3
+
+
+#reading .......................
+fhand = open('mbox-short.txt')
+count = 0
+for line in fhand:
+    count = count + 1
+print('Line Count:', count)
+
+# reading the whole file
+>>> fhand = open('mbox-short.txt')
+>>> inp = fhand.read()
+>>> print(len(inp))
+94626
+>>> print(inp[:20])
+From stephen.marquar
+
+# print all the lines start with From: and strip off the newline in the file
+fhand = open('mbox-short.txt')
+for line in fhand:
+    line = line.rstrip()
+    if line.startswith('From:'):
+        print(line)
+
+# Code: http://www.py4e.com/code3/search2.py
+
+# bad file names handling
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    quit()
+count = 0
+for line in fhand:
+    if line.startswith('Subject:'):
+        count = count + 1
+print('There were', count, 'subject lines in', fname)
+
+# Code: http://www.py4e.com/code3/search7.py
