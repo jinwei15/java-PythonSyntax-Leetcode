16dc0c71d67c861a18f3fd23a83466915c10d649
jinwei14 <janviezhang14@gmail.com>
2018-09-25 18:50:36 +0100

    adding to the dictionart

--- python/dictionary.py
+++ python/dictionary.py
@@ -34,3 +34,27 @@ d = dict()
 for c in word:
     d[c] = d.get(c,0) + 1
 print(d)
+
+
+# We will write a Python program to read through the lines of the file,
+# break each line into a list of words, and then loop through each of the words
+# in the line and count each word using a dictionary
+
+
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+
+counts = dict()
+for line in fhand:
+    words = line.split()
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+
+print(counts)
