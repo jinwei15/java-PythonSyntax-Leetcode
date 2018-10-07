074398e67d4e5532801b076987190e55c4b0e97f
jinwei14 <janviezhang14@gmail.com>
2018-10-07 10:57:29 +0100

    tuples2

--- python/tuple.py
+++ python/tuple.py
@@ -1,7 +1,21 @@
- This is week 6 tuples:
+# This is week 6 tuples:
 
-# looks just like list but we use partences
+# looks just like list but we use partences 
+
+#index start from 0 as well.
 
 x=('gem'.'sally','John')
+
+print(x[2])
+
+y=(1,2.3)
+print(y)
+print(max(y))
+
+for iter in y:
+	print(iter)
+
+
+
 
 
