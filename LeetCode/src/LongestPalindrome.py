03bc3d5a9f4e5416353a38470a2fdf77993e20c8
jinwei14 <janviezhang14@gmail.com>
2018-10-10 21:02:52 +0100

    string palindrome 2

--- LeetCode/src/LongestPalindrome.py
+++ LeetCode/src/LongestPalindrome.py
@@ -21,3 +21,16 @@ class Solution(object):
                 centre = 1
 
         return pair+centre
+
+
+        """
+class Solution:
+    def longestPalindrome(self, s):
+        ans = 0
+        for v in collections.Counter(s).itervalues():
+            ans += v / 2 * 2
+            if ans % 2 == 0 and v % 2 == 1:
+                ans += 1
+        return ans
+
+        """
