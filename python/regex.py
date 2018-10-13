c8536993afaeb7170d67f1f400dc3a1ce730dac7
jinwei14 <janviezhang14@gmail.com>
2018-10-13 15:52:59 +0100

    python regular expression

--- python/regex.py
+++ python/regex.py
@@ -1,11 +1,52 @@
 # regular expression
-import re 
+import re
 
-# re.search() to used to see if a string match a regular expression 
-#similar using find() 
+# re.search() to used to see if a string match a regular expression
+#similar using find()
 
 #re.findall() is like extration finding and pulling out.
 
-hand = open ('ddd.txt')
-for line in hand :
-	line = line
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    if re.search('From:', line):
+        print(line)
+
+
+"""
+is the replace of:::
+
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    if line.find('From:') >= 0:
+        print(line)
+"""
+
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    if re.search('^From:', line):  # the ^F mean I want the F form the begining of the line
+        print(line)
+
+"""
+is the replace of:::
+
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    if line.startswith('From'):
+        print(line)
+"""
+
+# -------------------------------------------------------------------------------------------------
+# . means any character
+# * means zero or more
+
+# ^X.*:  it means I am looking a word that start with X follow by any number of characters follow by colum
+
+
+"""
+X-adfhjkah:
+X-safdf$$$:
+"""
