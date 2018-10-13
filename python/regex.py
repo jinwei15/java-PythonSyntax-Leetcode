3b6426d0ac0cc6ad937e73ca368a6a410cf49a9f
jinwei14 <janviezhang14@gmail.com>
2018-10-13 15:46:19 +0100

    python regular expression

---
+++ python/regex.py
@@ -0,0 +1,11 @@
+# regular expression
+import re 
+
+# re.search() to used to see if a string match a regular expression 
+#similar using find() 
+
+#re.findall() is like extration finding and pulling out.
+
+hand = open ('ddd.txt')
+for line in hand :
+	line = line
