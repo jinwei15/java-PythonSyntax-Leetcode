8ae92ee01710ebf02f2a9c6c6d1e6a6155ec48f9
jinwei14 <janviezhang14@gmail.com>
2018-11-09 22:58:14 +0000

    try vim rubish

---
+++ LeetCode/src/productOfArrayExceptSelf.py
@@ -0,0 +1,11 @@
+class Solution:
+	def productExceptSelf(self,nums):
+	"""
+	:type nums:List[int]
+	:rtype:List[int]
+	"""
+	sumAll = sum(nums)
+	for i in nums:
+		result.append(sum-1)
+	return result
+ 
