892d369a6f121ad8498b6cc98f23c323ef8c39e0
jinwei14 <janviezhang14@gmail.com>
2018-12-14 14:54:33 +0000

    add one

---
+++ LeetCode/src/ReverseLinkedList.py
@@ -0,0 +1,45 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+"""
+        #put everying in a list
+        numberList = list()
+        while (head != None):
+            numberList.append(head.val)
+            head = head.next
+        
+        # reverse the list
+        newList = numberList[::-1]
+        
+        if len(newList) == 0: return None
+        #create a new List node.
+        newhead = ListNode(newList[0])
+        current = newhead
+        for i in range(1,len(newList)):
+            
+            node = ListNode(newList[i])
+            current.next = node
+            current = current.next
+        
+        return newhead
+    
+    
+  """
+
+	#ther msut be a better way.
+	          
+            
+            
+            
+            
+            
+        
