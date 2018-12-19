a2cd68cef8f3f0fefdf94c3822f48cbddfe173d0
jinwei14 <janviezhang14@gmail.com>
2018-12-19 07:02:27 +0000

    dict counter

---
+++ LeetCode/src/SubdomainVisitCount.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subdomainVisits(self, cpdomains):
+        """
+        :type cpdomains: List[str]
+        :rtype: List[str]
+        """
+        countPair = dict()
+        countResult = list()
+        for item in cpdomains:
+            elements = item.split(' ')
+            countPair[elements[1]] = countPair.get(elements[1],0) + int(elements[0])
+            while '.' in elements[1]:
+                index = elements[1].find('.')
+                elements[1] = elements[1][index+1:]
+                countPair[elements[1]] = countPair.get(elements[1],0) + int(elements[0])
+        for key, value in countPair.items():
+            item = str(value)+ ' '+ key  
+            countResult.append(item)
+        return countResult
+            
+    
