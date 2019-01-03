d14916ae4bcd8ab74a65e6bde7419d13bb0486b7
jinwei14 <janviezhang14@gmail.com>
2019-01-03 11:04:30 +0000

    array

---
+++ LeetCode/src/IslandPerimeter.py
@@ -0,0 +1,55 @@
+# ou are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water.
+
+# Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+# The island doesn't have "lakes" (water inside that isn't connected to the water around the island). One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+ 
+
+# Example:
+
+# Input:
+# [[0,1,0,0],
+#  [1,1,1,0],
+#  [0,1,0,0],
+#  [1,1,0,0]]
+
+# Output: 16
+
+# Explanation: The perimeter is the 16 yellow stripes in the image below:
+
+
+
+class Solution:
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        row = len(grid)
+        col = len(grid[0])
+        bigGrid = [[0 for x in range(col+2)] for y in range(row+2)] 
+        
+        for i in range(row):
+            for j in range(col):
+                bigGrid[i+1][j+1] = grid[i][j]
+        
+        # print(bigGrid)
+        
+        result = 0 
+        for i in range(1,row+1):
+            for j in range(1,col+1):
+                # up = i -1 
+                # down = i + 1
+                # left = j - 1
+                # right = j + 1
+                temp = 4
+                if bigGrid[i][j] == 1:
+                    sumAll = bigGrid[i -1][j]+bigGrid[i + 1][j]+bigGrid[i][j-1]+bigGrid[i][j+1]
+                    temp -= sumAll
+                    result += temp
+        return result
+
+                        
+                    
+                    
