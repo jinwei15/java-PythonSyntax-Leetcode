c7521f4b1b5be737e7e4afa0c5fd08341ff5e06c
jinwei.zhang <janviezhang14@gmail.com>
2019-02-05 23:41:57 +0000

    Update NumberofIslands.py

--- LeetCode/src/NumberofIslands.py
+++ LeetCode/src/NumberofIslands.py
@@ -85,6 +85,43 @@ class Solution:
     
 
         return num_islands;
+    
+class Solution:
+    def numIslands(self, grid: 'List[List[str]]') -> 'int':
+        if grid == None or len(grid) == 0:
+            return 0
+        
+        nr = len(grid)
+        nc = len(grid[0])
+        num_island = 0
+        queue = collections.deque()
+        for r in range(nr):
+            for c in range(nc):
+                if grid[r][c] == '1':
+                    num_island += 1
+                    
+                    queue.clear()
+                    queue.append((r,c))
+                    grid[r][c] = '0' # mark as 0
+                    while(len(queue) != 0):
+                        (row, col) = queue.popleft()
+                        if row - 1 >= 0 and grid[row-1][col] == '1':
+                            queue.append((row -1,col))
+                            grid[row-1][col] = '0'
+                        if row + 1 < nr and grid[row + 1][col] == '1':
+                            queue.append((row+1,col))
+                            grid[row+1][col] = '0'
+                        if col - 1 >= 0 and grid[row][col -1] == '1':
+                            queue.append((row,col-1))
+                            grid[row][col-1] = '0'
+                        if col + 1 < nc and grid[row][col +1] == '1':
+                            queue.append((row,col+1))
+                            grid[row][col+1] = '0'
+                            
+                        
+                        
+        return num_island
+                        
   
 
 
