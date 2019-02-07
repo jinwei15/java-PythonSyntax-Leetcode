42eadc01b80ce79ff0b1a97247e70f527c2cc05f
jinwei14 <janviezhang14@gmail.com>
2019-02-07 16:59:25 +0000

    mn

--- LeetCode/src/MissingNumber.py
+++ LeetCode/src/MissingNumber.py
@@ -1,19 +1,18 @@
 class Solution:
-#     def missingNumber(self, nums):
-#         """
-#         :type nums: List[int]
-#         :rtype: int
-#         """
-#         nums.sort()
-#         for i in range(len(nums)):
-#             if i != nums[i]:
-#                 return i
-#             if i == len(nums)-1:
-#                 return i+1
- 
-     def missingNumber(self, nums):
+    #     def missingNumber(self, nums):
+    #         """
+    #         :type nums: List[int]
+    #         :rtype: int
+    #         """
+    #         nums.sort()
+    #         for i in range(len(nums)):
+    #             if i != nums[i]:
+    #                 return i
+    #             if i == len(nums)-1:
+    #                 return i+1
+
+    def missingNumber(self, nums):
         num_set = set(nums)
-        n = len(nums) + 1
-        for number in range(n):
+        for number in range(len(nums) + 1):
             if number not in num_set:
                 return number
