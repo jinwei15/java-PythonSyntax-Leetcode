116e410064c37d874656e21e57959a9e179e3c03
jinwei14 <janviezhang14@gmail.com>
2019-04-12 16:42:00 +0100

    reverse string recursive

--- LeetCode/src/ReverseString.py
+++ LeetCode/src/ReverseString.py
@@ -4,5 +4,42 @@ class Solution:
         :type s: str
         :rtype: str
         """
+        left, right = 0, len(s)-1
+        while(left<right):
+            s[left] , s[right] = s[right],s[left]
+            left += 1
+            right -= 1
+            
+        
+
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         """
+#         Do not return anything, modify s in-place instead.
+#         """
+        
+#         def helper(s,start,end):
+#             if start >= end:
+#                 return
+
+#             s[start],s[end] = s[end],s[start]
+#             helper(s,start+1,end-1)
+            
+            
+        
+#         helper(s,0,len(s)-1)
+        
+        
+            
+        
+        
+        
+
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
         return s[::-1]
         
