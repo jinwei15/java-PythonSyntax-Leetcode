f56962e45a4365962be279f927f266bde0b8da02
jinwei14 <janviezhang14@gmail.com>
2019-04-14 11:10:19 +0100

    reverse Linked list

--- LeetCode/src/ReverseLinkedList.py
+++ LeetCode/src/ReverseLinkedList.py
@@ -38,4 +38,16 @@ class Solution:
             prev = curr
             curr = nextTemp
         return prev
-   
+
+
+class Solution:
+    def reverseList(self, head: 'ListNode') -> 'ListNode':
+        if head == None or head.next == None:
+            return head
+        p = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return p
+        
+        
+           
