38b7c5eedb6917af3bad59336b8511e4e40e3096
jinwei14 <janviezhang14@gmail.com>
2019-05-05 13:11:25 +0100

    adding code for 162

---
+++ LeetCode/src/FindPeakElement.py
@@ -0,0 +1,59 @@
+# 162. Find Peak Element
+# Medium
+#
+# 780
+#
+# 1241
+#
+# Favorite
+#
+# Share
+# A peak element is an element that is greater than its neighbors.
+#
+# Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+#
+# The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+#
+# You may imagine that nums[-1] = nums[n] = -∞.
+#
+# Example 1:
+#
+# Input: nums = [1,2,3,1]
+# Output: 2
+# Explanation: 3 is a peak element and your function should return the index number 2.
+# Example 2:
+#
+# Input: nums = [1,2,1,3,5,6,4]
+# Output: 1 or 5
+# Explanation: Your function can return either index number 1 where the peak element is 2,
+#              or index number 5 where the peak element is 6.
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if not nums: return None
+        if len(nums) == 1: return 0
+        left = 0
+        right = len(nums) - 1
+        while(left<=right):
+            mid = (left + right) // 2
+            if mid == 0:
+                if nums[mid]>nums[mid+1] :
+                    return mid
+                else:
+                    left = mid + 1
+            elif mid == len(nums) - 1:
+                if nums[mid-1]<nums[mid]:
+                    return mid
+                else:
+                    right = mid - 1
+            else:
+                if nums[mid-1]<nums[mid]>nums[mid+1]:
+                    return mid
+                elif nums[mid-1]<nums[mid]<nums[mid+1]:
+                    left = mid + 1
+                else: # nums[mid-1]>nums[mid]>nums[mid+1]: nums[mid-1]>nums[mid]<nums[mid+1]:
+                    right = mid - 1
+
+
+        return mid
