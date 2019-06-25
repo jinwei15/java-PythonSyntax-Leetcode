5b38ba81eb62b621ba944845dd5f020cf7ab95bc
jinwei14 <janviezhang14@gmail.com>
2019-06-25 10:05:24 +0100

    addind code for cracking

--- LeetCode/src/URLify.py
+++ LeetCode/src/URLify.py
@@ -4,12 +4,19 @@
 # output 'Mr%20John%20Smith'
 
 class Solution:
-    def URLify(self,string,number):
-        ret = string.rsplit()
+    def URLify(self, string, number):
+        print(string)
+        print(number)
+        ret = string.rstrip()
         ret.replace(' ','%20')
 
 
-        return ret
+        return ret.replace(' ','%20')
 
-    if __name__== "__main__":
-        URLify('Mr John Smith    ', 5)
+    # if __name__== "__main__":
+    #     # URLify('Mr John Smith    ', 5)
+
+
+a = Solution()
+
+print(a.URLify('Mr John Smith    ',5))
