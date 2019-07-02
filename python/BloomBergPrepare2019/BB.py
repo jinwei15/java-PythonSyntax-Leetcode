9dc6e55953c0807d14e6f2cd30b49522901b1733
jinwei14 <janviezhang14@gmail.com>
2019-07-02 20:40:58 +0100

    clear

--- python/BloomBergPrepare2019/BB.py
+++ python/BloomBergPrepare2019/BB.py
@@ -2,10 +2,14 @@
 
 
 
-
+LC 42.Trapping rain water
+LC 56 Merge Intervals
 LC 126. Word Ladder
 LC 127. Word Ladder
 LC 269. Alien Dictionary
+LC 235. Lowest Common Ancestor of a Binary Search Tre
 LC 703. Kth Largest Element in a Stream
+LC 706. Design HashMap
+LC 723. Candy Crush
 
 """
