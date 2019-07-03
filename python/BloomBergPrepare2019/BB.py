54cd0d73846ad61d5e470265cdb73cf8dab6ae35
jinwei14 <janviezhang14@gmail.com>
2019-07-03 09:40:29 +0100

    leetcode pre

--- python/BloomBergPrepare2019/BB.py
+++ python/BloomBergPrepare2019/BB.py
@@ -1,15 +1,35 @@
 """
 
 
-
-LC 42.Trapping rain water
-LC 56 Merge Intervals
-LC 126. Word Ladder
-LC 127. Word Ladder
-LC 269. Alien Dictionary
-LC 235. Lowest Common Ancestor of a Binary Search Tre
-LC 703. Kth Largest Element in a Stream
-LC 706. Design HashMap
-LC 723. Candy Crush
+3. Longest Substring Without Repeating Characters
+27. Remove Element
+42. Trapping rain water
+49. Group Anagrams
+56. Merge Intervals *2
+71. Simplify Path
+102 Binary Tree Level Order Traversal
+110. Balanced Binary Tree
+117
+126. Word Ladder
+127. Word Ladder
+138	Copy List with Random Pointer
+146. LRU Cache *2
+206	 Reverse Linked List
+226 Invert Binary Tree
+239
+253. Meeting Rooms II
+269. Alien Dictionary
+283. move zeros
+235. Lowest Common Ancestor of a Binary Search Tre
+314. Binary Tree Vertical Order Traversal 987 *2
+347. Top K Frequent Elements https://zpjiang.me/2017/11/13/top-k-elementes-system-design/
+445. Add Two Numbers II
+503. Next Greater Element II
+545
+387. First Unique Character in a String
+703. Kth Largest Element in a Stream
+706. Design HashMap
+723. Candy Crush
+1047. Remove All Adjacent Duplicates In String
 
 """
